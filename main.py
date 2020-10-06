74146a25cfbeff3a0e1710004932b1c1cfcb48ec
Cmcclellan1738 <69971600+Cmcclellan1738@users.noreply.github.com>
2020-10-06 09:16:31 -0400

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -52,8 +52,8 @@ bird_facing_right = img("""
 """)
 bird_facing_left = img("""
     . . . . . . . . . . . . f . . .
-    . . . . . . . . . . f f 2 f . .
-    . . . . f f . . . f 2 2 2 2 f .
+    . . . . . . . . . . . f 2 f . .
+    . . . . f f . . . f f 2 2 2 f .
     . . . f 2 2 f f f 2 2 f 2 f . .
     . . f 2 2 f 2 2 2 2 f f 2 f . .
     . f 4 2 2 2 2 2 2 f 2 2 f . . .
--- main.ts
+++ main.ts
@@ -54,8 +54,8 @@ let bird_facing_right = img`
 `
 let bird_facing_left = img`
     . . . . . . . . . . . . f . . .
-    . . . . . . . . . . f f 2 f . .
-    . . . . f f . . . f 2 2 2 2 f .
+    . . . . . . . . . . . f 2 f . .
+    . . . . f f . . . f f 2 2 2 f .
     . . . f 2 2 f f f 2 2 f 2 f . .
     . . f 2 2 f 2 2 2 2 f f 2 f . .
     . f 4 2 2 2 2 2 2 f 2 2 f . . .
